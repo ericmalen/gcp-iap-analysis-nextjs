7afd33857ab36ab3ae5dd8b9c729ba0342739dd8
ericmalen <71109944+ericmalen@users.noreply.github.com>
2024-01-14 22:47:36 -0400

    removed hard coded headers

--- gcp-iap-analysis/app/ui/headers/table.tsx
+++ gcp-iap-analysis/app/ui/headers/table.tsx
@@ -14,32 +14,7 @@ export default async function HeadersTable({
   headers: [string, string][]
 }) {
   const offset = ITEMS_PER_PAGE * (currentPage - 1);
-  //const filteredHeaders = getFilteredHeaders(query);
-  const filteredHeaders = [
-    [ 'host', 'localhost:3000' ],
-    [
-      'user-agent',
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20100101 Firefox/121.0'
-    ],
-    [ 'accept', '*/*' ],
-    [ 'accept-language', 'en-CA,en-US;q=0.7,en;q=0.3' ],
-    [ 'accept-encoding', 'gzip, deflate, br' ],
-    [ 'referer', 'http://localhost:3000/dashboard/headers?page=3' ],
-    [ 'next-url', '/dashboard/headers' ],
-    [ 'dnt', '1' ],
-    [ 'connection', 'keep-alive' ],
-    [
-      'cookie',
-      'authjs.csrf-token=e0a10aa8a08c9da90237182a6206d463b6bad4961e320806aa094c10f6fcaf8f%7C46940bb756d6f5c2680531620bc76273dc8831f186f3cd2d6ec39b1753883fad; authjs.callback-url=http%3A%2F%2Flocalhost%3A3000%2Flogin%3FcallbackUrl%3Dhttp%253A%252F%252Flocalhost%253A3000%252Fdashboard; authjs.session-token=eyJhbGciOiJkaXIiLCJlbmMiOiJBMjU2R0NNIn0..4GN6vPNM3EGAGFHV.7_8cPqLFBtYJNRYdAe3_vbnCxXsVf5-7Mg2uVDv8dhaQOCudgaqEPXXZrF-wat_1O6kR12_NZq9-ScQrRU9knWMdOEWw_G7eO88DuZqx2LrB4pG6Gr73GTi9XYIyCTh7SZP9Hk0J_C8Ea-YOJahD9wfVYa37xF0Z8lG9M0L2BPlqvL2qdUy68B-x-sGulL_1G93q_PuR_gKn3kxvPMvdMA53zlSgbhI.tfweelDusVTykX9nwxyWYg'
-    ],
-    [ 'sec-fetch-dest', 'empty' ],
-    [ 'sec-fetch-mode', 'cors' ],
-    [ 'sec-fetch-site', 'same-origin' ],
-    [ 'x-forwarded-host', 'localhost:3000' ],
-    [ 'x-forwarded-port', '3000' ],
-    [ 'x-forwarded-proto', 'http' ],
-    [ 'x-forwarded-for', '::ffff:127.0.0.1' ]
-  ]
+  const filteredHeaders = getFilteredHeaders(query);
 
   return (
     <div className="mt-6 flow-root">
